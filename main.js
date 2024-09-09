471928a9682eb4026377b9f71d8ef9076a386320
AndresTabor <andrestaborda43@gmail.com>
2024-09-09 21:27:52 +0200

    add validations and dynamic states

--- main.js
+++ main.js
@@ -47,9 +47,37 @@ const provincias = [
     "Zamora",
     "Zaragoza"
 ];
-const showStatesOptions = () =>{
+
+const formValidation = () => {
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
   
-  
+        form.classList.add('was-validated')
+      }, false)
+    })
 }
 
-document.addEventListener('DOMContentLoaded', showStatesOptions)
+const showStates = () => {
+    provincias.map( state => {
+        const optionElement = document.createElement("option")
+        const selectElement = document.getElementById("selectedStates")
+        optionElement.value = state;
+        optionElement.textContent = state;
+
+        selectElement.appendChild(optionElement);
+    })
+}
+
+const setUp = () =>{
+    formValidation()
+    showStates()
+}
+
+document.addEventListener('DOMContentLoaded', setUp)
